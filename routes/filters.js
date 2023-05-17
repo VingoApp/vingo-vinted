2d4eb7aefb5f4493316ffb76635937519d150553
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-05-17 20:13:36 +0200

    try to fix

--- routes/filters.js
+++ routes/filters.js
@@ -10,6 +10,7 @@ const insertToDb = require("../controllers/filter/insertToDb");
 router.get("/combo", [passport.authenticate('jwt', { session: false }), rateLimit], async (req, res) => {
     console.log("✨ POST /filters/combo")
     const comboList = req.query?.comboList;
+    console.log(comboList)
     if (!comboList) return res.status(400).json({ success: false, msg: "Missing arguments" })
     const filter = await getSpecificFromDb(comboList);
     if (!filter) return res.status(400).json({ success: false, msg: "Error while fetching data from DB" })
